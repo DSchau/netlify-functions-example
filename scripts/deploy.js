ba32b952bbbe86f671a5da0663fecbec528c73a0
Dustin Schau <dustinschau@gmail.com>
2020-02-20 19:32:59 -0800

    chore: update log

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -19,8 +19,8 @@ async function publish() {
   await client.deploy(`1870766c-a150-416a-9db6-c0a2f548261f`, `public`, {
     fnDir: `functions`,
     syncFileLimit: 500,
-    statusCb(...args) {
-      console.log(args)
+    statusCb(update) {
+      console.log(update)
     }
   })
 }
