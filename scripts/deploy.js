a903b199fedf0a6f5cb23d417c773349a7e8c100
Dustin Schau <dustinschau@gmail.com>
2020-02-20 19:22:17 -0800

    feat: initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const Netlify = require('netlify')
+const path = require('path')
+
+const client = new Netlify(process.env.ACCESS_TOKEN)
+
+async function createSite() {
+  await client.createSiteInTeam({
+    body: {
+      name: `netlify-functions-test-dschau`
+    },
+    accountSlug: `dschau`
+  })
+}
+async function publish() {
+  await client.deploy(`1870766c-a150-416a-9db6-c0a2f548261f`, `public`, {
+    fnDir: path.join('public', 'functions'),
+    syncFileLimit: 500,
+    statusCb(...args) {
+      console.log(args)
+    }
+  })
+}
+
+publish()
