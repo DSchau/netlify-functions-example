64a706686e0ab93ff84df60533a423ccdca48c00
Dustin Schau <dustinschau@gmail.com>
2020-02-20 19:30:39 -0800

    chore: updatae

--- functions/hello.js
+++ functions/hello.js
@@ -2,7 +2,7 @@ exports.handler = async function (event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello from the server`
+      message: `👋 from the server`
     })
   }
 }
