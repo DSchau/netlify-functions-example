927700c245314fbaa963b1b3825733c4aaa65dbe
Dustin Schau <dustinschau@gmail.com>
2020-02-20 19:32:35 -0800

    chore: args

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,12 +7,15 @@ const client = new Netlify(process.env.ACCESS_TOKEN)
 async function createSite() {
   await client.createSiteInTeam({
     body: {
+      // TODO: your site name
       name: `netlify-functions-test-dschau`
     },
+    // TODO: your account slug
     accountSlug: `dschau`
   })
 }
 async function publish() {
+  // TODO: your siteID
   await client.deploy(`1870766c-a150-416a-9db6-c0a2f548261f`, `public`, {
     fnDir: `functions`,
     syncFileLimit: 500,
